3629825fae633c5a301f94c91e070283bc4b2755
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-08 16:33:56 -0300

    implementando exceção no código

--- ProjetoCelular/Celular/Main.java
+++ ProjetoCelular/Celular/Main.java
@@ -1,13 +1,26 @@
 package ProjetoCelular.Celular;
+import java.util.Scanner;
+
 import ProjetoCelular.Sistema.Sistema;
 
 public class Main{
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
     Sistema sistema = new Sistema();
 
+    try{
+      System.out.println("Deseja iniciar o sistema ? [S] / [N]");
+    String resposta = sc.next().toUpperCase();
+    if(resposta == "S"){
+
+    }
     System.out.println(sistema.rodarSistema()); // método de retorno 
 
+
+    }
+    catch ()
+    
     
     
   }
