c42a0537c7406d335855996a70efc5574e7136c4
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-08 18:11:16 -0300

    atualizando condicionais e loops

--- ProjetoCelular/Celular/Main.java
+++ ProjetoCelular/Celular/Main.java
@@ -39,54 +39,69 @@ public class Main{
           System.out.println();
           limpar.limparTela();
 
-
-          switch (num) {
-            case 1:
-              System.out.println(". . . Abrindo aplicativo de Camêra . . ."); Thread.sleep(2000);
-              sistema.tirarFoto(); Thread.sleep(1000);
-              sistema.gravarVideo(); Thread.sleep(1000);
-              sistema.armazenar(); Thread.sleep(1000);
-              System.out.println("-------------------------------------------");
-              break;
+          boolean loop = true;
+          while (loop){
+            switch (num) {
+              case 1:
+                System.out.println(". . . Abrindo aplicativo de Camêra . . ."); Thread.sleep(2000);
+                sistema.tirarFoto(); Thread.sleep(1000);
+                sistema.gravarVideo(); Thread.sleep(1000);
+                sistema.armazenar(); Thread.sleep(1000);
+                System.out.println("-------------------------------------------");
+                break;
+              
+              case 2:
+                System.out.println(". . . Abrindo aplicativo de Música . . ."); Thread.sleep(2000);
+                sistema.selecionarMusica(); Thread.sleep(1000);
+                sistema.tocarMusica(); Thread.sleep(1000);
+                sistema.pausarMusica(); Thread.sleep(1000);
+                sistema.proximaMusica(); Thread.sleep(1000);
+                sistema.anteriorMusica(); Thread.sleep(1000);
+                System.out.println("-------------------------------------------");
+                break;
+  
+              case 3:
+                System.out.println(". . . Abrindo o Navegador de Internet . . ."); Thread.sleep(2000);
+                sistema.acessarInternet(); Thread.sleep(1000);
+                sistema.adicionarAba(); Thread.sleep(1000);
+                sistema.atualizarPagina(); Thread.sleep(1000);
+                System.out.println("-------------------------------------------");
+                break;
+  
+              case 4:
+                System.out.println(". . . Abrindo lista de Contatos . . ."); Thread.sleep(2000);
+                sistema.armazenarContato(); Thread.sleep(1000);
+                sistema.ligar(); Thread.sleep(1000);
+                sistema.iniciarCorreioVoz(); Thread.sleep(1000);
+                sistema.desligar(); Thread.sleep(1000);
+                System.out.println("-------------------------------------------");
+                break;
+  
+              case 5:
+                System.out.println(". . . Abrindo GPS . . ."); Thread.sleep(2000);
+                sistema.acessarCoordenadas(); Thread.sleep(1000);
+                sistema.fornecerLocalizacao(); Thread.sleep(1000);
+                System.out.println("-------------------------------------------");
+                break;
             
-            case 2:
-              System.out.println(". . . Abrindo aplicativo de Música . . ."); Thread.sleep(2000);
-              sistema.selecionarMusica(); Thread.sleep(1000);
-              sistema.tocarMusica(); Thread.sleep(1000);
-              sistema.pausarMusica(); Thread.sleep(1000);
-              sistema.proximaMusica(); Thread.sleep(1000);
-              sistema.anteriorMusica(); Thread.sleep(1000);
-              System.out.println("-------------------------------------------");
-              break;
+              default:
+                System.out.println("Digite um número entre 1 - 5 ");
+                break;
+            }
 
-            case 3:
-              System.out.println(". . . Abrindo o Navegador de Internet . . ."); Thread.sleep(2000);
-              sistema.acessarInternet(); Thread.sleep(1000);
-              sistema.adicionarAba(); Thread.sleep(1000);
-              sistema.atualizarPagina(); Thread.sleep(1000);
-              System.out.println("-------------------------------------------");
-              break;
-
-            case 4:
-              System.out.println(". . . Abrindo lista de Contatos . . ."); Thread.sleep(2000);
-              sistema.armazenarContato(); Thread.sleep(1000);
-              sistema.ligar(); Thread.sleep(1000);
-              sistema.iniciarCorreioVoz(); Thread.sleep(1000);
-              sistema.desligar(); Thread.sleep(1000);
-              System.out.println("-------------------------------------------");
-              break;
-          
-            default:
-              break;
           }
+          
         }
         else if (resposta == 'n'){
           System.out.println("Desligando o celular . . .");
-          for(int i=0; i<10; i++){
-            String[] vetor = new String[i];
-            vetor[i] = "-";
+
+          String[] vetor = new String[10];
+      
+
+          for(int i=0; i<vetor.length; i++){
+            vetor[i] += "teste";
             System.out.println(vetor[i]);
-            vetor[i] += "-";
+            
           }
           System.out.println("-");
           ligado = false;
