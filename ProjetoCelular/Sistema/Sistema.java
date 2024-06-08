4e3205f2f575eb017c3fd365da1f6b0545eeb609
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-08 15:44:19 -0300

    implementando todas as classes

--- ProjetoCelular/Sistema/Sistema.java
+++ ProjetoCelular/Sistema/Sistema.java
@@ -1,6 +1,6 @@
 package ProjetoCelular.Sistema;
 
-public class Sistema implements Camera, NavegadorInternet{
+public class Sistema implements Camera, NavegadorInternet, Gps, Telefonar, ReprodutorMusical{
 
   @Override
   public void tirarFoto() {
@@ -18,6 +18,72 @@ public class Sistema implements Camera, NavegadorInternet{
   public void armazenar() {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'armazenar'");
+  }
+
+  @Override
+  public void tocarMusica() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'tocarMusica'");
+  }
+
+  @Override
+  public void pausarMusica() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'pausarMusica'");
+  }
+
+  @Override
+  public void proximaMusica() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'proximaMusica'");
+  }
+
+  @Override
+  public void anteriorMusica() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'anteriorMusica'");
+  }
+
+  @Override
+  public void armazenarContato() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'armazenarContato'");
+  }
+
+  @Override
+  public void ligar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+  }
+
+  @Override
+  public void desligar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'desligar'");
+  }
+
+  @Override
+  public void discar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'discar'");
+  }
+
+  @Override
+  public void acessarCoordenadas() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'acessarCoordenadas'");
+  }
+
+  @Override
+  public void fornecerLocalizacao() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'fornecerLocalizacao'");
+  }
+
+  @Override
+  public void acessarInternet() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'acessarInternet'");
   }
   
 }
