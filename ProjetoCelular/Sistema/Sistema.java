4b92871b20f9acd1fd0220d86916000d5d080649
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-08 15:42:55 -0300

    implementando camera

---
+++ ProjetoCelular/Sistema/Sistema.java
@@ -0,0 +1,23 @@
+package ProjetoCelular.Sistema;
+
+public class Sistema implements Camera{
+
+  @Override
+  public void tirarFoto() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'tirarFoto'");
+  }
+
+  @Override
+  public void gravarVideo() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'gravarVideo'");
+  }
+
+  @Override
+  public void armazenar() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'armazenar'");
+  }
+  
+}
