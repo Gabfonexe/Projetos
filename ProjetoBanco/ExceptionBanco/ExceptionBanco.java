66d5f2614ff0f49e9ebe61e2e470de7324fa517d
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-14 16:03:41 -0300

    implementando o exception personalizado

---
+++ ProjetoBanco/ExceptionBanco/ExceptionBanco.java
@@ -0,0 +1,11 @@
+package ProjetoBanco.ExceptionBanco;
+
+public class ExceptionBanco extends Exception{
+
+  private static final long serialVersionUID = 1L;
+
+  public ExceptionBanco(String msg){
+    super(msg);
+  }
+  
+}
